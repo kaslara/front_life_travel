a4aa16974065bc7a6e3c7b46a0747ee907208c5a
Ardittz <arditmullabazi@gmail.com>
2022-10-10 01:06:33 +0200

    Added search functionality

--- travelephant/components/bus.js
+++ travelephant/components/bus.js
@@ -1,12 +1,10 @@
 function Bus({bus}){
     return (
-        <>
        <a class=" block m-2 p-6 max-w-full bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
         <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Bus Company: {bus.name}</h5>
          <p class="font-normal text-gray-700 dark:text-gray-400">From: {bus.departure}</p>
          <p class="font-normal text-gray-700 dark:text-gray-400">To: {bus.destination}</p>
        </a>
-         </>
     )
 }
 export default Bus
